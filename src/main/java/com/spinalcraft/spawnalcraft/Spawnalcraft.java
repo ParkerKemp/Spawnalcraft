d4c7c86ec56be8e888acc6dc9dd5d24460a304cd
Parker Kemp <charles.kemp702@gmail.com>
2016-11-25 01:48:36 -0500

    Removed redundant init code.

--- src/main/java/com/spinalcraft/spawnalcraft/Spawnalcraft.java
+++ src/main/java/com/spinalcraft/spawnalcraft/Spawnalcraft.java
@@ -36,8 +36,6 @@ public class Spawnalcraft extends SpinalcraftPlugin implements Listener{
 	public void onEnable(){
 		console = Bukkit.getConsoleSender();
 		
-		console.sendMessage(ChatColor.BLUE + "Spawnalcraft online!");
-		getDataFolder().mkdirs();
 		getMOTD();
 		saveDefaultConfig();
 		loadConfig();
